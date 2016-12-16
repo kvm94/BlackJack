c572239e6d625a5aa4413793e54310eafc079cb8
dechefVincent <dechef_vincent@hotmail.be>
2016-12-17 00:50:10 +0100

    UPDATE: User class

--- src/Beans/User.java
+++ src/Beans/User.java
@@ -1,5 +1,63 @@
 package Beans;
 
-public class User {
+import java.time.LocalDate;
 
+public class User {
+	
+	private String login;
+	private String password;
+	private String name;
+	private String firstName;
+	private LocalDate birthDate;
+	private String mail;
+	private int capital;
+	
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+	
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	
+	public int getCapital() {
+		return capital;
+	}
+	public void setCapital(int capital) {
+		this.capital = capital;
+	}
 }
