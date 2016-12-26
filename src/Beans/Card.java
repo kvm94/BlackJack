414979d9fb92ba8f5af6632eeba2eb9859d45bf1
dechefVincent <dechef_vincent@hotmail.be>
2016-12-26 22:15:15 +0100

    UPDATE : Card .Bean

--- src/Beans/Card.java
+++ src/Beans/Card.java
@@ -1,5 +1,28 @@
 package Beans;
 
 public class Card {
+	public Sign sign;
+    public Rank rank;
+    public int 	value;
+    
+	public Sign getSign() {
+		return sign;
+	}
+	public void setSign(Sign sign) {
+		this.sign = sign;
+	}
 	
+	public Rank getRank() {
+		return rank;
+	}
+	public void setRank(Rank rank) {
+		this.rank = rank;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
 }
