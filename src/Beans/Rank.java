ca7291b82ea14bba3cc9974f67b45c97c907125f
dechefVincent <dechef_vincent@hotmail.be>
2016-12-26 22:14:48 +0100

    ADD: Rank & Sign .Enum

---
+++ src/Beans/Rank.java
@@ -0,0 +1,17 @@
+package Beans;
+
+public enum Rank {
+	As, 
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King
+}
---
+++ src/Beans/Sign.java
@@ -0,0 +1,8 @@
+package Beans;
+
+public enum Sign {
+    Spade, 	// = Pique
+    Tile, 	// = Carreau
+    Clover,	// = Trèfle
+    Heart	// = Coeur
+}
