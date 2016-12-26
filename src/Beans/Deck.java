8213fba54e9794bff1df1a5977cdc1077a99d3cb
dechefVincent <dechef_vincent@hotmail.be>
2016-12-26 22:18:31 +0100

    UPDATE : Deck .Beans

--- src/Beans/Deck.java
+++ src/Beans/Deck.java
@@ -1,5 +1,14 @@
 package Beans;
 
+import java.util.List;
+
 public class Deck {
-	
+	private List<Card> cards;
+
+	public List<Card> getCards() {
+		return cards;
+	}
+	public void setCards(List<Card> cards) {
+		this.cards = cards;
+	}
 }
