673bc9462a60ccf71a4e979cec9e8d09f167f136
dechefVincent <dechef_vincent@hotmail.be>
2016-12-31 02:59:12 +0100

    CLEAN : beans

--- src/Beans/Game.java
+++ src/Beans/Game.java
@@ -4,53 +4,58 @@ import java.time.LocalDate;
 import java.util.List;
 
 public class Game {
-	
-	private int			id;
-	private LocalDate 	date;
-	private int 		nbrTurns;
-	private int			resultGame;
-	private List<Turn> 	turns;
-	private User		user;
-	
+
+	private int id;
+	private LocalDate date;
+	private int nbrTurns;
+	private int resultGame;
+	private List<Turn> turns;
+	private User user;
+
+	public LocalDate getDate() {
+		return date;
+	}
 
 	public int getId() {
 		return id;
 	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	
-	public LocalDate getDate() {
-		return date;
-	}
-	public void setDate(LocalDate date) {
-		this.date = date;
-	}
-	
+
 	public int getNbrTurns() {
 		return nbrTurns;
 	}
-	public void setNbrTurns(int nbrTurns) {
-		this.nbrTurns = nbrTurns;
-	}	
-	
+
 	public int getResultGame() {
 		return resultGame;
 	}
-	public void setResultGame(int resultGame) {
-		this.resultGame = resultGame;
-	}
-	
+
 	public List<Turn> getTurns() {
 		return turns;
 	}
-	public void setTurns(List<Turn> turns) {
-		this.turns = turns;
-	}
-	
+
 	public User getUser() {
 		return user;
 	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setNbrTurns(int nbrTurns) {
+		this.nbrTurns = nbrTurns;
+	}
+
+	public void setResultGame(int resultGame) {
+		this.resultGame = resultGame;
+	}
+
+	public void setTurns(List<Turn> turns) {
+		this.turns = turns;
+	}
+
 	public void setUser(User user) {
 		this.user = user;
 	}
--- src/Beans/Turn.java
+++ src/Beans/Turn.java
@@ -3,69 +3,77 @@ package Beans;
 import java.util.List;
 
 public class Turn {
-	
-	private int			id;
-	private int		 	win;
-	private int 		croupierScore;
-	private int 		userScore;
-	private int			bet;
-	private Deck		deck;
-	private List<Card> 	userHand;
-	private List<Card>	croupierHand;
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	
-	public int isWin() {
-		return win;
-	}
-	public void setWin(int win) {
-		this.win = win;
-	}
-	
-	public int getCroupierScore() {
-		return croupierScore;
-	}
-	public void setCroupierScore(int croupierScore) {
-		this.croupierScore = croupierScore;
-	}
-	
-	public int getUserScore() {
-		return userScore;
-	}
-	public void setUserScore(int userScore) {
-		this.userScore = userScore;
-	}
-	
-	public int getBet() {
+
+	private int id;
+	private int win;
+	private int croupierScore;
+	private int userScore;
+	private double bet;
+	private Deck deck;
+	private List<Card> userHand;
+	private List<Card> croupierHand;
+
+	public double getBet() {
 		return bet;
 	}
-	public void setBet(int bet) {
-		this.bet = bet;
-	}
-	
-	public Deck getDeck() {
-		return deck;
-	}
-	public void setDeck(Deck deck) {
-		this.deck = deck;
-	}	
-	
-	public List<Card> getUserHand() {
-		return userHand;
-	}
-	public void setUserHand(List<Card> userHand) {
-		this.userHand = userHand;
-	}
-	
+
 	public List<Card> getCroupierHand() {
 		return croupierHand;
 	}
+
+	public int getCroupierScore() {
+		return croupierScore;
+	}
+
+	public Deck getDeck() {
+		return deck;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public List<Card> getUserHand() {
+		return userHand;
+	}
+
+	public int getUserScore() {
+		return userScore;
+	}
+
+	public int isWin() {
+		return win;
+	}
+
+	public void setBet(double bet) {
+		this.bet = bet;
+	}
+
 	public void setCroupierHand(List<Card> croupierHand) {
 		this.croupierHand = croupierHand;
+	}
+
+	public void setCroupierScore(int croupierScore) {
+		this.croupierScore = croupierScore;
+	}
+
+	public void setDeck(Deck deck) {
+		this.deck = deck;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setUserHand(List<Card> userHand) {
+		this.userHand = userHand;
+	}
+
+	public void setUserScore(int userScore) {
+		this.userScore = userScore;
+	}
+
+	public void setWin(int win) {
+		this.win = win;
 	}
 }
